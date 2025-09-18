411f2410e8dcf70c36c1cda868fcc5a4b6ced995
ihercula <ihercula@alumnos.unex.es>
2025-09-18 18:32:48 +0200

    fablab

---
+++ src/main/java/com/uex/fablab/FablabApplication.java
@@ -0,0 +1,13 @@
+package com.uex.fablab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FablabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FablabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uex/fablab/FablabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uex.fablab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FablabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
