42dea63da92e9eeabca0ff41d2c35bdebcf13dd7
ihercula <ihercula@alumnos.unex.es>
2025-10-09 12:07:35 +0200

    bootstrap files and user model

---
+++ src/main/java/com/uex/fablab/model/User.java
@@ -0,0 +1,31 @@
+package com.uex.fablab.model;
+
+public class User {
+    private int id;
+    private String nombre;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
