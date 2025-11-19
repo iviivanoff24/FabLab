7848d7bb6706bc7b0c5a528cd7d6b2747cd24357
ihercula <ihercula@alumnos.unex.es>
2025-11-19 18:57:37 +0100

    x

--- src/main/java/com/uex/fablab/controller/HomeController.java
+++ src/main/java/com/uex/fablab/controller/HomeController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    // Prueba github
-    //Prueba CAMs
 
     @GetMapping("/")
     public ResponseEntity<Resource> index() {
