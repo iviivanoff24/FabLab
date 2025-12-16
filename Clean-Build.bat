59fc0f34660cd6a71cb525f7a50da3db366b57c8
ihercula <ihercula@alumnos.unex.es>
2025-12-16 18:48:06 +0100

    .

--- Clean-Build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-if exist target (
-    echo Borrando carpeta target...
-    rmdir /s /q target
-)
-mvnw clean package
-pause
--- run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-mvnw spring-boot:run
-pause
